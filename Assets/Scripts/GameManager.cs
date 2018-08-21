9e5a70167779c9bb4a85b5097455cb51ff393638
Zach Oriel <Zacharyaaron15@gmail.com>
2018-08-21 12:17:39 -0700

    Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public ObjectSpawner objectSpawner;
+    public Camera topDownCam;
+    public GameObject grenadeSpawnPoint, grenadeObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (objectSpawner == null)
+        {
+            objectSpawner = GetComponent<ObjectSpawner>();
+        }
+
+        if (grenadeSpawnPoint.transform.parent == null)
+        {
+            grenadeSpawnPoint.transform.parent = topDownCam.gameObject.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.G) && topDownCam.enabled)
+        {
+            objectSpawner.GrenadeSpawn();
+        }
+	}
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+    GameManager gameManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        objectPooler = ObjectPooler.Instance;
+        gameManager = FindObjectOfType<GameManager>();
+	}
+	
+	public void GrenadeSpawn()
+    {
+        objectPooler.SpawnFromPool("Grenade", gameManager.grenadeSpawnPoint.transform.position, Quaternion.identity);
+    }
+}
